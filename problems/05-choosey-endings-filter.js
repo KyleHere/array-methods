650ee2ef9e871c604c17eb81fadf885ba5c498dc
Kyle Tseng <ktseng122@gmail.com>
2021-03-19 11:51:32 -0400

    Complete 05

--- problems/05-choosey-endings-filter.js
+++ problems/05-choosey-endings-filter.js
@@ -2,7 +2,7 @@
 Write a function `chooseyEndings` that accepts an array of words and a suffix
 string as arguments. The function should return a new array containing the words
 that end in the given suffix. If the value passed in is not an array, return an
-empty array. Solve this using Array's `filter()` method. 
+empty array. Solve this using Array's `filter()` method.
 
 HINT: There are built in JavaScript functions that will help with determining if
 a strings ends a certain way. Go see if you can find it on MDN!
@@ -26,7 +26,16 @@ console.log(chooseyEndings(17, 'ily'));
 */
 
 
-// your code here
+function chooseyEndings(arr, suffix){
+  let final = [];
+  if(!Array.isArray(arr)){
+    return final;
+  }
+  let chooseArray = arr.filter(function(el){
+    return el.endsWith(suffix);
+  })
+  return chooseArray;
+}
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
