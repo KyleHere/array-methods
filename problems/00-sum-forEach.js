14e017c321aaecc8c0859810124547ff8c59e478
Kyle Tseng <ktseng122@gmail.com>
2021-03-19 11:27:07 -0400

    Complete 00

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -11,7 +11,23 @@ console.log(sumForEach([6, 7, 2])); // 15
 console.log(sumForEach([])); // 0
 */
 
-// your code here
+//Understand
+  //function named sumForEach
+    //accepts an array (arr)
+  //returns the total sum of all values in the array
+  //solve using forEach method
+//Plan
+  //define sum = 0
+  //forEach
+    //sum += ar
+
+function sumForEach(arr){
+  let sum = 0;
+  arr.forEach(function(num){
+    sum += num;
+  })
+  return sum;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
